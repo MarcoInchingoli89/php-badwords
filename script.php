d727efc0456d19b680847e2e6f6a38c001b16585
Marco Inchingoli <evilmarkus@hotmail.it>
2022-11-25 16:32:13 +0100

    Add comment to script Final Commit

--- script.php
+++ script.php
@@ -9,6 +9,7 @@ echo $paragrafo . "<br>";
 echo "Il paragrafo è lungo " . strlen($paragrafo) . " caratteri" . "<br>";
 
 // Sostituisco la parola scritta con gli asterischi nel paragrafo e lo stampo a schermo con la sua lunghezza
+// Rendo la parola minuscola per censurare anche le parole maiuscole
 $paragrafoCensurato = str_replace(strtolower($parola), '***', $paragrafo) . "<br>";
 echo $paragrafoCensurato;
 echo "Il paragrafo censurato è lungo " . strlen($paragrafo) . " caratteri";
