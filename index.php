4188fe1441b10c2f8867e8390e9f18a0a81aeb52
Marco Inchingoli <evilmarkus@hotmail.it>
2022-11-25 15:22:19 +0100

    Initial Commit Add Form

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>PHP Badwords</title>
+</head>
+
+<body>
+    <form>
+        <div class="mb-3 p-3">
+            <label for="exampleInputEmail1" class="form-label">Parola da censurare</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3 p-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Paragrafo</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary ms-3">Submit</button>
+    </form>
+</body>
+
+</html>
